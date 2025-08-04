fe0c7b2613e7e787aa678bc92fdb16ebf3a1e404
2551247912 <2551247912@qq.com>
2025-08-05 00:44:41 +0800

    增加“赋值与算术操作符”示例。

---
+++ 赋值与算术操作符/assignment-and-arithmetic-operators.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    name = "li lei"
+    print("name:",name)
+
+    print("1 + 2",1 + 2)
+    print("1 - 2",1 - 2)
+    print("2 * 2",2 * 2)
+    print("8 / 2",8 / 2)
+    print("8 // 5",8 // 5)
+    print("8 % 5",8 % 5)
+    print("8  ** 2",8**2)
+    print("Hi " + name)
+    print("Hello~"*3)
+
+    number = 4
+    number *= 2
+    print("number:",number)
