9dca8abecfa89ec67aefea99cee5a097808b1df8
2551247912 <2551247912@qq.com>
2025-08-09 23:34:43 +0800

    增加“选择结构（if）”“三元操作符”示例。

---
+++ 三元操作符/if-2.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    x = 1
+    y = 2
+    smaller = x if x < y else y
+    print("smaller=",smaller)
---
+++ 选择结构/if-1.py
@@ -0,0 +1,27 @@
+if __name__ == '__main__':
+    x = 9
+
+    if x < 0:
+        print("x=",x)
+        print("It is negative number")
+
+    elif x > 0:
+        print("x=",x)
+        print("It is positive number")
+
+    else:
+        print("x=",x)
+        print("It is zero")
+
+
+    x=7
+    print("x=",x)
+
+    if x%2 == 1:
+        print("It is 奇数")
+    else:
+        print("It is 偶数")
+
+    x=345
+    if 100 <= x <= 999:
+        print("x=",x)
